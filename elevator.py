a00b30cc8cfa1e9e080f0ca33801cf4b97407a19
Jose Tiago Macara Coutinho <tiago.coutinho@crunch.io>
2025-07-03 14:24:09 +0200

    v3: basic reduce loop

--- elevator.py
+++ elevator.py
@@ -1,36 +1,27 @@
-
 def solve():
+    floors = 1, 2, 3, 4, 5
+    coopers = 2, 3, 4, 5
+    bakers = 1, 2, 3, 4
+    fletchers = 2, 3, 4
 
-  def baker(baker, floors):
-    return baker != 5
-  def cooper(cooper, floors):
-    return cooper != 1
-  def fletcher(fletcher, floors):
-    cooper = floors[1]
-    return fletcher > 1 and fletcher < 5 and fletcher not in (cooper - 1, cooper + 1) 
-  def miller(miller, floors):
-    cooper = floors[1]
-    return miller > cooper
-  def smith(smith, floors):
-    fletcher = floors[2]
-    return smith not in (fletcher - 1, fletcher + 1)
+    def fletcher(fletcher, floors):
+        cooper = floors[1]
+        return fletcher not in (cooper - 1, cooper + 1)
 
-  floors = 1, 2, 3, 4, 5
-  bakers = floors
-  coopers = floors
-  fletchers = floors
-  millers = floors
-  smiths = floors
-  for s in smiths:
-    for c in coopers:
-      for b in bakers:
-        for f in fletchers:
-          for m in millers:
-            comb = (b, c, f, m, s)
-            if len(set(comb)) != 5:
-                continue
-            if baker(b, comb) and cooper(c, comb) and fletcher(f, comb) and miller(m, comb) and smith(s, comb):
-                return comb
+    def smith(smith, floors):
+        fletcher = floors[2]
+        return smith not in (fletcher - 1, fletcher + 1)
+
+    for s in floors:
+        for c in coopers:
+            for b in bakers:
+                for f in fletchers:
+                    for m in floors[c:]:
+                        comb = (b, c, f, m, s)
+                        if len(set(comb)) != 5:
+                            continue
+                        if fletcher(f, comb) and smith(s, comb):
+                            return comb
 
 
 assert solve() == (3, 2, 4, 5, 1)
